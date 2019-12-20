cc26411350eb13c80930f74469e38260dc361649
Archit Raj <archit.raj@unisys.com>
2019-12-20 16:41:00 +0530

    first draft

---
+++ precache-manifest.ca8abf35d24e875c72fd9c749d24b043.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1491b106a94749561e64",
+    "url": "/static/css/main.cfb2b904.chunk.css"
+  },
+  {
+    "revision": "1491b106a94749561e64",
+    "url": "/static/js/main.1491b106.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "23071b3dfd6955423847bf01e5c2da2d",
+    "url": "/index.html"
+  }
+];
